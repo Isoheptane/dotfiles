f272c4f2b8ba94008146bd7095201b4f6baeb55c
Isoheptane <chomodiso@outlook.com>
2023-05-20 18:50:22 +0800

    Initial dotfiles

---
+++ update.sh
@@ -0,0 +1,11 @@
+# Hyprland
+cp ~/.config/hypr/hyprland.conf ./hyprland/
+# Waybar
+cp ~/.config/waybar/config ./waybar/
+cp ~/.config/waybar/style.css ./waybar/
+# mako
+cp ~/.config/mako/config ./mako/config
+# tofi
+cp ~/.config/tofi/config ./tofi/config
+# Alacritty
+cp ~/.config/alacritty/alacritty.yml ./alacritty/
