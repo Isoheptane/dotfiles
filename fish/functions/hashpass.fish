4152c3b69c9571cfabbbafccd7123dbc01c38956
Isoheptane <chomodiso@outlook.com>
2023-07-13 09:07:11 +0800

    Modify hashpass function

--- fish/functions/hashpass.fish
+++ fish/functions/hashpass.fish
@@ -1,6 +1,5 @@
 function hashpass
-	read password
-	echo "$password"
+	read -s password
 	set -f hash (printf $password | sha256sum | cut -d " " -f 1 | xxd -r -p | base64 | cut -b 32-)
-	echo "C7c$hash"
+	printf "C7c$hash" | wl-copy
 end
