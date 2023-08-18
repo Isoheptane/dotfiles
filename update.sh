40736443aa34a293d75473aeccada93a370d4e81
Isoheptane <chomodiso@outlook.com>
2023-08-19 02:27:16 +0800

    Ordinary update

--- update.sh
+++ update.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-cp -r ~/.config/hypr/*          ./hypr/
-cp -r ~/.config/waybar/*        ./waybar/
-cp -r ~/.config/mako/*          ./mako/
-cp -r ~/.config/tofi/*          ./tofi/
-cp -r ~/.config/alacritty/*     ./alacritty/
-cp -r ~/.config/fish/*          ./fish/
-cp -r ~/.config/fcitx5/*        ./fcitx5/
-cp -r ~/.config/fontconfig/*	./fontconfig
+rm -r ./hypr/
+cp -r ~/.config/hypr/          ./hypr/
+rm -r ./waybar/
+cp -r ~/.config/waybar/        ./waybar/
+rm -r ./mako/ 
+cp -r ~/.config/mako/          ./mako/
+rm -r ./tofi/
+cp -r ~/.config/tofi/          ./tofi/
+rm -r ./alacritty/
+cp -r ~/.config/alacritty/     ./alacritty/
+rm -r ./fish/  
+cp -r ~/.config/fish/          ./fish/
+rm -r ./fcitx5/
+cp -r ~/.config/fcitx5/        ./fcitx5/
+rm -r ./fontconfig
+cp -r ~/.config/fontconfig/	./fontconfig/
