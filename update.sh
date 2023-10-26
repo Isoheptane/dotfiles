55f5225b3cc74338afc979489ec7a88b181662ca
Isoheptane <chomodiso@outlook.com>
2023-10-27 00:54:21 +0800

    Remove borders and big changes

--- update.sh
+++ update.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 rm -r ./hypr/
-cp -r ~/.config/hypr/          ./hypr/
+cp -r ~/.config/hypr/					./hypr/
 rm -r ./waybar/
-cp -r ~/.config/waybar/        ./waybar/
+cp -r ~/.config/waybar/					./waybar/
 rm -r ./mako/ 
-cp -r ~/.config/mako/          ./mako/
+cp -r ~/.config/mako/					./mako/
 rm -r ./tofi/
-cp -r ~/.config/tofi/          ./tofi/
+cp -r ~/.config/tofi/					./tofi/
 rm -r ./alacritty/
-cp -r ~/.config/alacritty/     ./alacritty/
+mkdir ./alacritty/
+cp ~/.config/alacritty/alacritty.yml	./alacritty/
 rm -r ./fish/  
-cp -r ~/.config/fish/          ./fish/
+cp -r ~/.config/fish/					./fish/
 rm -r ./fcitx5/
-cp -r ~/.config/fcitx5/        ./fcitx5/
+cp -r ~/.config/fcitx5/					./fcitx5/
 rm -r ./fontconfig
-cp -r ~/.config/fontconfig/	./fontconfig/
+cp -r ~/.config/fontconfig/				./fontconfig/
