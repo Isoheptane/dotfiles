86f535d567603c07c0c60cad02db2d9cd717c002
Isoheptane <chomodiso@outlook.com>
2023-12-13 19:30:22 +0800

    Fcitx5 & FontConfig & Hyprland & tofi

---
+++ fish/conf.d/fish-esp-idf.fish
@@ -0,0 +1,2 @@
+alias get-idf 'source ~/.local/lib/esp/esp-idf/export.fish'
+export IDF_TOOLS_PATH=$HOME/.local/lib/esp/.espressif/
--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,4 @@
 if status is-interactive
+	export GPG_TTY=$(tty)
     # Commands to run in interactive sessions can go here
 end
