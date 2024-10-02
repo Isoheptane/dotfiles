ed427fc3acbfa75456754031a1ab06edbbbfa270
Isoheptane <chomodiso@outlook.com>
2024-10-03 00:16:12 +0800

    Regular Update on 2024/10/13

--- hypr/switch_workspace.sh
+++ hypr/switch_workspace.sh
@@ -8,10 +8,10 @@ workspace_id=$(($monitor_id * 10 + $workspace))
 echo "Final Operation: $operation to $workspace_id"
 
 if [[ $operation == "switch" ]]; then
-	# hyprctl dispatch moveworkspacetomonitor $workspace_id $monitor_id;
+	hyprctl dispatch moveworkspacetomonitor $workspace_id $monitor_id;
 	hyprctl dispatch workspace $workspace_id;
 fi
 if [[ $operation == "move" ]]; then
-	# hyprctl dispatch moveworkspacetomonitor $workspace_id $monitor_id;
+	hyprctl dispatch moveworkspacetomonitor $workspace_id $monitor_id;
 	hyprctl dispatch movetoworkspace $workspace_id;
 fi
