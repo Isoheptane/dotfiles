96dcd2c69e56a0808de7b0ebce8c25e462636cbc
Isoheptane <chomodiso@outlook.com>
2026-06-10 22:21:28 +0800

    Updated hyprland lua script

--- fish/conf.d/fish-esp-idf.fish
+++ fish/conf.d/fish-esp-idf.fish
@@ -1,2 +1,1 @@
-alias get-idf "source $HOME/.local/lib/esp/esp-idf/export.fish"
-export IDF_TOOLS_PATH=$HOME/.local/lib/esp/.espressif/
+alias get-idf "source $HOME/.espressif/v6.0/esp-idf/export.fish"
